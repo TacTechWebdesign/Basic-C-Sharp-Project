fd0ff22be4afd0bfa28b1a503e7f7a882d70c367
TacTechWebdesign <118140620+TacTechWebdesign@users.noreply.github.com>
2023-03-24 12:17:21 +0800

    Branching Assignment
    Console-based application for getting a shipping quote for a package

---
+++ BranchingAss.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package is too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    int quote = weight * width * height * length / 100;
+                    decimal shipping = quote;
+
+                    Console.WriteLine("Your estimate total for shipping this package is: " + shipping.ToString("C"));
+                    Console.WriteLine("Thank you");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
